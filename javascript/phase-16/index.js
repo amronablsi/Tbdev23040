243521f2bc444a7ba18b5cb1db245a6508d45d35
amronablsi <amr.nablsi@gmail.com>
2023-07-27 11:46:56 +0200

    modif 27/07

--- javascript/phase-16/index.js
+++ javascript/phase-16/index.js
@@ -1,9 +1,34 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    var selectedOption = document.getElementById("environnement").value;
-    if (selectedOption === "") {
-      document.getElementById("environnement-error").style.display = "block";
-      event.preventDefault();
-    } else {
-      document.getElementById("environnement-error").style.display = "none";
-    }
-  });
+function verif() {
+  alert('gne');
+  var societe = document.getElementById('societe').value;
+  var contact = document.getElementById('contact').value;
+  var postal = document.getElementById('postal').value;
+  var ville = document.getElementById('ville').value;
+  var email = document.getElementById('mail').value;
+
+  var mailVerif = RegExp('^[a-z0-9.-]+@[a-z0-9.-]{2,}.[a-z]{2,4}$');
+  var lettre = RegExp('^[a-zA-Z]+$');
+  var postalVerif = RegExp('^[0-9]{5}$');
+  
+
+  if (lettre.test(societe) == false) {
+      alert('Veuillez saisir un nom de société correct.');
+      return;
+  }
+  if (lettre.test(contact) == false) {
+      alert('Veuillez saisir un contact correct.');
+      return;
+  }
+  if (postalVerif.test(postal) == false){
+      alert('Veuillez saisir un code postal correct');
+      return;
+  }
+  if (lettre.test(ville) == false) {
+      alert('Veuillez saisir un nom de ville correct.');
+      return;
+  }
+  if (mailVerif.test(email) == false) {
+      alert('Veuillez saisir un Email correct.');
+      return;
+  }
+}
